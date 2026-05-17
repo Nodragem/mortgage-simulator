675d7ea5d51fe437b096a8da2f27c0c74d7d0335
Geoffrey Mégardon <geoffrey.megardon@gmail.com>
2026-05-17 15:48:36 +0100

    change graph to plotly

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -19,14 +20,14 @@ N = mortgage_term*12.0 # total number of month of the fix rate
 years = np.arange(mortgage_term+1)
 
 
-def get_monthly_repayment(r, P, N):
+def get_monthly_repayment():
     return (r*P) / (1-(1+r)**(-N)) 
 
 def repayment_curve(years, overpayment_month=0):
     # c: monthly_repayment
     # n: number of months
     n = years * 12
-    c = get_monthly_repayment(r, P, N) + overpayment_month
+    c = get_monthly_repayment() + overpayment_month
     # see https://en.wikipedia.org/wiki/Mortgage_calculator
     result = (1+r)**n * P - (((1+r)**n - 1)* c )/r
     result[result < 0] = 0
@@ -34,33 +35,27 @@ def repayment_curve(years, overpayment_month=0):
 
 
 c1, c2, c3 = st.columns([2,1,1])
-c1.write(f"Monthly Payment: :blue[{round(get_monthly_repayment(r, P, N),2)}]")
+c1.write(f"Monthly Payment: :blue[£{round(get_monthly_repayment(),2)}]")
 
 c2.write("No Overpayment:")
 debt1 = pd.DataFrame({"years": years, "debt": repayment_curve(years)})
 c2.table(debt1)
 debt1["scenario"] = "no overpayment"
 
-c3.write(f"Overpayment of {overpayment}")
+c3.write(f"Overpayment of £{overpayment}")
 debt2 = pd.DataFrame({"years": years, "debt": repayment_curve(years, overpayment)})
 c3.table(debt2, height="stretch")
 debt2["scenario"] = f"£{overpayment} per month"
 
 df = pd.concat([debt1, debt2])
 
-# st.line_chart(df, x="years", y="debt", color='scenario', markers=True)
 
 c1.write("Remaining Debt per year:")
-c1.vega_lite_chart(
-    df,
-    {
-        "mark": {"type": "line","point": True},
-    "encoding": {
-        "x": {"field": "years", "type":"quamtitative"},
-        "y": {"field": "debt", "type": "quantitative"},
-        "color": {"field": "scenario", "type": "nominal"}
-        },
-    "config": {
-    "legend": {"orient": "top", "layout": {"top": {"anchor": "left"}}}
-  }
-    }, width=800, height=500)
+fig = px.line(df, x="years", y="debt", color='scenario', markers=True)
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="right",
+    x=0.99
+))
+c1.plotly_chart(fig,width=800, height=500)
